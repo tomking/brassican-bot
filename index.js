7c82bded021095cd5a066bb48ebe24b161c108ed
tomking <tomking127@gmail.com>
2023-08-19 13:27:31 -0400

    Add initial bot boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
