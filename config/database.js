302e831221a0393f3b3642fa991abd123a1c7db4
tomking <tomking127@gmail.com>
2023-08-21 14:15:12 -0400

    Implement specific database connection

--- config/database.js
+++ config/database.js
@@ -1,9 +1,10 @@
 MONGO_URL = process.env.MONGO_URL;
+CABBAGE_DB_NAME = process.env.CABBAGE_DB_NAME;
 
 const mongoose = require('mongoose');
 
 mongoose
-    .connect(MONGO_URL)
+    .connect(MONGO_URL + '/' + CABBAGE_DB_NAME)
     .catch((error) => console.error('Database connection failed: ', error));
 
 module.exports = mongoose;
