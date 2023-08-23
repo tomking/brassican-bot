fbcaf35ca5879c06fa17610f972c3a3b078e63f3
tomking <tomking127@gmail.com>
2023-08-23 19:16:40 -0400

    Change ping command description

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with Pong!'),
+        .setDescription('Wondering if the servers are on fire?'),
     async execute(interaction) {
         await interaction.reply(
             `Pong! (Websocket heartbeat: ${interaction.client.ws.ping}ms.)`
