13336cc260fa6a5d0aac2f49c827ca54e9645197
tomking <thomas@tomking.io>
2024-10-22 12:51:41 -0400

    Pull value out to constant

--- config/wom.js
+++ config/wom.js
@@ -1,15 +1,17 @@
 const { WOMClient } = require('@wise-old-man/utils');
 const { Environment } = require('../services/environment');
 
+const WOM_RATE_LIMIT = 100;
+
 let womClient;
-let availTokens = 100;
+let availTokens = WOM_RATE_LIMIT;
 let lastTokenReplenish = Date.now();
 
 const replenishTokens = () => {
     const now = Date.now();
     const elapsedTime = now - lastTokenReplenish;
-    const tokensToAdd = Math.floor((elapsedTime / 60000) * 100);
-    availTokens = Math.min(availTokens + tokensToAdd, 100);
+    const tokensToAdd = Math.floor((elapsedTime / 60000) * WOM_RATE_LIMIT);
+    availTokens = Math.min(availTokens + tokensToAdd, WOM_RATE_LIMIT);
     lastTokenReplenish = now;
 };
 
@@ -26,7 +28,7 @@ const requestWithRateLimit = async (apiCall) => {
     if (canMakeCall()) {
         return apiCall();
     } else {
-        const delay = 60000 / 100;
+        const delay = 60000 / WOM_RATE_LIMIT;
         return new Promise((resolve) =>
             setTimeout(() => resolve(requestWithRateLimit(apiCall)), delay)
         );
