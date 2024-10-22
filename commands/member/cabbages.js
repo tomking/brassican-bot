ed6726fb2204ab9d9bbdedf3abe217ffcccc1624
DisneyManBro <56454265+DisneyManBro@users.noreply.github.com>
2024-10-22 21:23:18 +0200

    Use user's globalName is nickname isn't available

--- commands/member/cabbages.js
+++ commands/member/cabbages.js
@@ -51,7 +51,7 @@ const mobileBreakdown = (member, memberData) => {
     const cabbages = Math.floor(memberData.currentCabbages);
     const cabbageBreakdown = getCabbageBreakdown(memberData);
     const timestamp = Math.floor(Date.parse(memberData.updatedAt) / 1000);
-    const nickname = capitalize(member.nickname);
+    const nickname = capitalize(member.nickname ?? member.user.globalName);
     const nextTierAmount = cabbagesUntilNext(cabbages);
     const nextTierText = nextTierAmount > 0 ? nextTierAmount.toString() : 'N/A';
     // Generate text block
@@ -198,7 +198,7 @@ const cabbageEmbed = (member, memberData) => {
         cabbagesText.push(cabbageBreakdown.clogSlots);
     }
     // Generate embed
-    const nickname = capitalize(member.nickname);
+    const nickname = capitalize(member.nickname ?? member.user.globalName);
     const nextTierAmount = cabbagesUntilNext(cabbages);
     const nextTierText = nextTierAmount > 0 ? nextTierAmount.toString() : 'N/A';
     const embed = new EmbedBuilder()
