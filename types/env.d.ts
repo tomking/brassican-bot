949c729e8e2f7b25dc80f63e9ca66b46f83f79df
tomking <thomas@tomking.io>
2024-12-23 22:47:16 -0500

    Add custom env var typings

---
+++ types/env.d.ts
@@ -0,0 +1,26 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        CABBAGE_DB_NAME: string;
+        DEVELOPER_DISCORD_CONTACT: string;
+        DIAMOND_RANK_ID: string;
+        DISCORD_APP_ID: string;
+        DISCORD_BOT_TOKEN: string;
+        DISCORD_CA_ROLE_ID: string;
+        DISCORD_MOD_ROLE_ID: string;
+        DRAGON_STONE_RANK_ID: string;
+        EMERALD_RANK_ID: string;
+        GUILD_ID: string;
+        JADE_RANK_ID: string;
+        LOG_CHANNEL_ID: string;
+        MONGO_URL: string;
+        ONYX_RANK_ID: string;
+        ONYX_RANK_ID: string;
+        RED_TOPAZ_RANK_ID: string;
+        RUBY_RANK_ID: string;
+        SAPPHIRE_RANK_ID: string;
+        WOM_API_KEY: string;
+        WOM_GROUP_ID: string;
+        WOM_GROUP_VERIFICATION_CODE: string;
+        ZENYTE_RANK_ID: string;
+    }
+}
