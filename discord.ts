f70dad9d5c83163308bb4ec807ccc80774fce59d
tomking <thomas@tomking.io>
2024-12-28 16:21:04 -0500

    Remove extra command removal.

--- discord.ts
+++ discord.ts
@@ -91,29 +91,7 @@ export const initialize = async () => {
             `Started refreshing ${client.commands?.size || 0} application (/) commands.`
         );
 
-        // Delete all existing global commands
-        await rest
-            .put(Routes.applicationCommands(Environment.DISCORD_APP_ID), {
-                body: [],
-            })
-            .then(() => console.log('Successfully deleted all guild commands.'))
-            .catch(console.error);
-
-        // Delete all existing guild commands
-        await rest
-            .put(
-                Routes.applicationGuildCommands(
-                    Environment.DISCORD_APP_ID,
-                    Environment.GUILD_ID
-                ),
-                {
-                    body: [],
-                }
-            )
-            .then(() => console.log('Successfully deleted all guild commands.'))
-            .catch(console.error);
-
-        // The put method is used to fully refresh all commands with the current set.
+        // The put method is used to fully redeploy all commands with the current set.
         const data = await rest.put(
             Routes.applicationCommands(Environment.DISCORD_APP_ID),
             {
